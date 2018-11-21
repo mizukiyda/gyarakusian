37b575bd8e482e6d4cfe29e90e1a37a6d466f903
GM2180136\nwuser <mizuki060411@gmail.com>
2018-11-21 15:24:37 +0900

    {add}  UIのソース
    中山瑞基

---
+++ Sorce/UI.cpp
@@ -0,0 +1,24 @@
+#include "DxLib.h"
+#include "UI.h"
+#include "Player.h"
+
+
+int UI_Init() {
+
+	return 0;
+}
+
+int UI_Dept() {
+
+	return 0;
+}
+
+int UI_Draw() {
+
+	return 0;
+}
+
+int UI_End() {
+
+	return 0;
+}
