810da9c5b44f6bafa3e25c287414a94706b84236
足立拓海 <takumiadachi096@gmail.com>
2018-11-30 15:32:46 +0900

    change

--- Sorce/Enemy.h
+++ Sorce/Enemy.h
@@ -2,9 +2,15 @@
 #define INCLUDE_ENEMY
 #define RIGHT 1
 #define LEFT -1
+#define NUMSHOT 4
+#define EnemyCount 48
 
 extern int Enemy_Init();
 extern int Enemy_Move();
+extern int Enemy_Move_Flg(int *);
+extern int Enemy_Attack_Chose(int *);
+extern int Enemy_Attack_Move(int *);
+extern int Enemy_Shot(int,int,int);
 extern int Enemy_Draw();
 extern int Enemy_Pos_Init_x();
 extern int Enemy_Pos_Init_y();
@@ -13,8 +19,8 @@ extern int Enemy_End();
 
 struct P_Enemy {
 	int Type;   //0:黄色　1:赤　2:紫　3:青
-	double x;	//座標
-	double y;
+	int x;	//座標
+	int y;
 	double fx;	//first_x 初期位置
 	double fy;
 	double nx;	//next_x 次のXの場所	
