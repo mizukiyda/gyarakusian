d7bc47a9748a105347892f239cc81a9f0f6f7089
ishikrua <ishikura.yda@gmail.com>
2018-12-03 12:36:14 +0900

    [update] サウンド修正
    サウンドとその他の修正

--- Sorce/Sound.h
+++ Sorce/Sound.h
@@ -2,9 +2,11 @@
 #ifndef INCLUDE_SOUND
 #define INCLUDE_SOUND
 
-extern int Sound_Init();
-extern int Sound_Dpct();
+void Sound_Init();
+void Sound_Dpct();
 extern int Sound_Draw();
 extern int Sound_End();
+
+void SetGax_Sound(int flag);
 
 #endif
