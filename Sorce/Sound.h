47a73f224a6b858070ac2650d1cc7002f88b1140
ishikrua <ishikura.yda@gmail.com>
2018-12-06 10:52:59 +0900

    [update] サウンド更新
    サウンドの更新

--- Sorce/Sound.h
+++ Sorce/Sound.h
@@ -2,8 +2,8 @@
 #ifndef INCLUDE_SOUND
 #define INCLUDE_SOUND
 
-void Sound_Init();
-void Sound_Dpct();
+extern int Sound_Init();
+extern int Sound_Dpct();
 extern int Sound_Draw();
 extern int Sound_End();
 
